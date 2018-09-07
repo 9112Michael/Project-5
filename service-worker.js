aaffe05b468a172bb30f4df8ea124244d38bd7a7
9112Michael <sitkabikemike@yahoo.com>
2018-09-07 10:41:28 -0700

    updated service worker 'fetch'

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,12 @@
 const cacheNameIs = "ver1";
 const stuffToCache = [
-  '/',
+    '/',
     '/index.html',
     '/restaurant.html',
     '/css/styles.css',
     '/js/dbhelper.js',
     '/js/main.js',
     '/js/restaurant_info.js',
-    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
     '/data/restaurants.json',
     '/img/1.jpg',
     '/img/2.jpg',
@@ -19,7 +17,11 @@ const stuffToCache = [
     '/img/7.jpg',
     '/img/8.jpg',
     '/img/9.jpg',
-    '/img/10.jpg'
+    '/img/10.jpg',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
+   
 ];
 
 //https://developers.google.com/web/fundamentals/primers/service-workers/
@@ -36,25 +38,46 @@ self.addEventListener('install', (event) => {
   );
 });
 
-//Cache and return requests (...making your app offline-first!)
-//https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+https://matthewcranford.com/restaurant-reviews-app-walkthrough-part-4-service-workers/
 self.addEventListener('fetch', (event) => {
+
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        {
-          return response||fetch(event.request)
-          .then((response) => {
-            const cloneResponse = response.clone();
-            caches.open(cacheNameIs).then((cache) => {
-              cache.put(event.request,cloneResponse);
-            })
-            return response;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
+      caches.match(event.request).then((response) => {
+
+          if (response) {
+              return response;
           }
+
+          else {
+              return fetch(event.request)
+                  .then((response) => {
+                      const cloneResponse = response.clone();
+                      caches.open('ver1').then((cache) => {
+                          cache.put(event.request, cloneResponse);
+                      })
+                      return response;
+                  })
+                  .catch((err) => {
+                      console.error(err);
+                  });
+          }         
       })
-    )})
+  );
+});
+
+
+
+//Update service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheNameIs) => {
+        }).map((stuffToCache) => {
+          return caches.delete(stuffToCache);
+        })
+      );
+    })
+  );
+});
+
