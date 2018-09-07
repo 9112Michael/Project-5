ce6d216e255dcd460ce9d2e590074b945ec197af
9112Michael <sitkabikemike@yahoo.com>
2018-09-07 15:36:39 -0700

    Changes to variables in restaurant HTML required by  first review

--- js/main.js
+++ js/main.js
@@ -176,11 +176,11 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt=restaurant.alt;//Images must have alternate text
+  image.alt=restaurant.name;//Images must have alternate text
                           // 1194.22 (a) Text equivalent for non-text elements
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
